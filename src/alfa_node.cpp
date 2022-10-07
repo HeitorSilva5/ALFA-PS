658a400c86950ea19ff3e8af4edf89cfe5a1d032
Heitor Silva <heitorsilva5711@gmail.com>
2022-10-07 12:41:52 +0100

    angle corre 5

--- src/alfa_node.cpp
+++ src/alfa_node.cpp
@@ -58,10 +58,10 @@ void AlfaNode::store_pointcloud_hardware(pcl::PointCloud<pcl::PointXYZI>::Ptr in
     for (auto point :*input_cloud){
         //elevation
         elevation = (float) ((std::atan2(point.z, std::hypot(point.x, point.y)))* (180.0f/M_PI)) *RES_MULTIPLIER;
-        a16_points[0] = elevation;
+        a16_points[1] = elevation;
         //azimuth
         const auto a = std::atan2(point.y, point.x);
-        a16_points[1] = (float) ((point.y >= 0 ? a : a + M_PI * 2) * (180.0f/M_PI)) *RES_MULTIPLIER;
+        a16_points[0] = (float) ((point.y >= 0 ? a : a + M_PI * 2) * (180.0f/M_PI)) *RES_MULTIPLIER;
         //range
         a16_points[2] = std::sqrt(point.x * point.x + point.y * point.y + point.z * point.z) *RES_MULTIPLIER;
         memcpy((void*)(pointer+pointcloud_index),a16_points,sizeof(int16_t)*3);
