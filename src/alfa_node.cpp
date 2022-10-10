73298df50d49f28e63b2faa3444b1cb173720990
Heitor Silva <heitorsilva5711@gmail.com>
2022-10-10 15:45:05 +0100

    ahn 2

--- src/alfa_node.cpp
+++ src/alfa_node.cpp
@@ -53,10 +53,9 @@ void AlfaNode::store_pointcloud_hardware(pcl::PointCloud<pcl::PointXYZI>::Ptr in
 {
     int pointcloud_index = 0;
     uint16_t a16_points[4];
-    a16_points[0] = 0;
-    //a16_points[3]=0;
     int16_t elevation;
     for (auto point :*input_cloud){
+        a16_points[0] = 0;
         //elevation
         elevation = (float) ((std::atan2(point.z, std::hypot(point.x, point.y)))* (180.0f/M_PI)) *RES_MULTIPLIER;
         a16_points[1] = elevation;
@@ -68,7 +67,7 @@ void AlfaNode::store_pointcloud_hardware(pcl::PointCloud<pcl::PointXYZI>::Ptr in
         memcpy((void*)(pointer+pointcloud_index),a16_points,sizeof(int16_t)*3);
         if(pointcloud_index < 16 || (pointcloud_index>=32 && pointcloud_index <=47) || (input_cloud->size()-pointcloud_index<10))
         {   
-            std::cout << pointcloud_index << "ELEVATION: " << elevation << " | a16_PTS: " << a16_points[0] << "  " << a16_points[1] << "  " << a16_points[2] << endl;
+            std::cout << pointcloud_index << "ELEVATION: " << elevation << " | a16_PTS: " << a16_points[1] << "  " << a16_points[2] << "  " << a16_points[3] << endl;
         }
         pointcloud_index++;
     }
